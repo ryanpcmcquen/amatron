b68f74d0b1786e2dd5e05c700a437e36cebe51d8
Ryan P.C. McQuen <ryan.q@linux.com>
2016-05-20 06:44:31 -0700

    Use more es6 syntax.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*! amatron v0.1.2 by ryanpcmcquen */
+/*! amatron v0.1.3 by ryanpcmcquen */
 /*global require, process*/
 /*jslint white:true, es6:true*/
 
@@ -14,8 +14,8 @@ const BrowserWindow = electron.BrowserWindow;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-(function () {
-  let createWindow = function () {
+(() => {
+  const createWindow = () => {
     // Create the browser window.
     mainWindow = new BrowserWindow({
       title: 'amatron'
@@ -30,7 +30,7 @@ let mainWindow;
     //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', () => {
       // Dereference the window object, usually you would store windows
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
@@ -43,7 +43,7 @@ let mainWindow;
   app.on('ready', createWindow);
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', function () {
+  app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -51,7 +51,7 @@ let mainWindow;
     }
   });
 
-  app.on('activate', function () {
+  app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
@@ -59,4 +59,4 @@ let mainWindow;
     }
   });
 
-}());
+})();
